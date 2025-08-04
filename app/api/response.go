4021eb8940d0e70f78d15f3ad90891427798a760
Eduard Heredia <eduard.heredia@deporvillage.com>
2025-08-04 20:42:20 +0200

    Implement response

--- app/api/response.go
+++ app/api/response.go
@@ -1,11 +1,35 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func OKResponse(w http.ResponseWriter, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 func ErrorResponse(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	response := map[string]string{
+		"error": message,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err := w.Write([]byte("Internal server error"))
+		if err != nil {
+			return
+		}
+		return
+	}
 }
