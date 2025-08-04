3b657207bb852985d95f9eb0e14159b92d6aa188
Eduard Heredia <eduard.heredia@deporvillage.com>
2025-08-04 20:44:07 +0200

    simplify response code

--- app/api/response.go
+++ app/api/response.go
@@ -7,29 +7,19 @@ import (
 
 func OKResponse(w http.ResponseWriter, data any) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func ErrorResponse(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
-	response := map[string]string{
-		"error": message,
-	}
-
+	response := map[string]string{"error": message}
 	if err := json.NewEncoder(w).Encode(response); err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte("Internal server error"))
-		if err != nil {
-			return
-		}
-		return
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
